3304e9c5a179ba92ba89edb7a426ab3ea41efca6
Lam Nguyen <tunglam030699@gmail.com>
2023-03-31 02:15:22 -0400

    authentication setup

--- app/models.py
+++ app/models.py
@@ -1,12 +1,23 @@
+from enum import unique
 from http import server
 from sqlalchemy import *
 from .database import Base
 
 class Post(Base):
-    __tablename__ = 'post'
+    __tablename__ = 'posts'
 
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    pubished = Column(Boolean, server_default='TRUE', nullable=False)
-    create_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
